7d2d4c48b3b413438a3493bfa261cafed9e18152
Weronika Niemczyk <weronika.tlusciak@gmail.com>
2023-04-15 17:23:12 +0200

    added css pagination

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { searchMovies } from './js/searchMovies';
 import './sass/main.scss';
-
+import { createPagination } from './js/pagination'; // *dodałam to tutaj tylko na potrzeby testów, aby pojawiał się znacznik paginacji kiedy nie ma jeszcze pozostałych części, końcowo należy * //
 
 searchMovies;
 
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -4,9 +4,8 @@ import 'tui-pagination/dist/tui-pagination.css';
 const paginationContainer = document.querySelector('#pagination');
 
 // * funkcja musi zostać zaimportowana przy renderowaniu galerii i wyszukiwaniu przez słowa kluczowe * //
-// * dopiero wtedy będę mogła zająć się jej wyglądem i dopracowaniem - nie wiem czy działa * //
 
-export function createPagination(totalItems, visiblePages) {
+createPagination = (totalItems, visiblePages) => {
   const options = {
     totalItems: totalItems,
     itemsPerPage: 20,
@@ -15,5 +14,7 @@ export function createPagination(totalItems, visiblePages) {
   const pagination = new Pagination(paginationContainer, options);
 
   return pagination;
-  
-}
+};
+console.log(createPagination(200, 5));
+
+export { createPagination };

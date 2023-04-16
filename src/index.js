86851d300dee3115cf9f3658d1b01ff87246226a
Weronika Niemczyk <weronika.tlusciak@gmail.com>
2023-04-16 14:08:02 +0200

    css updated

--- src/index.js
+++ src/index.js
@@ -3,4 +3,3 @@ import './sass/main.scss';
 import { createPagination } from './js/pagination'; // *dodałam to tutaj tylko na potrzeby testów, aby pojawiał się znacznik paginacji kiedy nie ma jeszcze pozostałych części, końcowo należy * //
 
 searchMovies;
-

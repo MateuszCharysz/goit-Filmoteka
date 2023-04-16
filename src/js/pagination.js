c82d3647cf88fdee14c0c393bb56e8ddee24ca74
Weronika Niemczyk <weronika.tlusciak@gmail.com>
2023-04-16 14:41:38 +0200

    markup pagination function added

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -18,3 +18,35 @@ createPagination = (totalItems, visiblePages) => {
 console.log(createPagination(200, 5));
 
 export { createPagination };
+
+// rendering movie cards //
+
+// import fetch get trending - na końcu musi być '&page=${page}' //
+const pagination = createPagination(data.total_results, data.total_pages);
+
+// KEY WORDS //
+//przy api keywords potrzebuje na końcu dodać '&page=${page}'
+// potrzebuje osobnej funkcji jak np.:
+// async function getByKeyword(name, page) {
+//     const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${name}&page=${page}`; //*/ name powinno być poza stałą API_URL
+//     return await axios
+//       .get(url)
+//       .then(response => {
+//         return response.data;
+//       })
+//       .catch(error => console.log(error));
+//   }
+
+// const pagination = createPagination(data.total_results, data.total_pages);
+// pagination.on(({page //z fetch movies //
+// }) => {
+//     //query selector do galerii//
+//     .innerHTML = '';
+//     //funkcja obsługująca fetcha key words//
+//     (name, page.then(data => {
+//        //funkcja obsługująca fetcha key words//
+//        .innerHTML =
+//        //funkcja - markup galerii //(data.results);
+//     }))
+// })
+// to wszystko powinno się zawierać w funkcji, która szuka po słowie kluczowym //

06034f1dd59478429b5183a088b047d7e8f06acc
Szymon1021 <105548800+Szymon1021@users.noreply.github.com>
2023-04-21 09:31:32 +0200

    Feature/localStorage

--- src/js/localStorage/localStorage.js
+++ src/js/localStorage/localStorage.js
@@ -3,6 +3,7 @@
     przechowywanej w pamięci lokalnej przeglądarki dla zalogowanego użytkownika. 
     Jeśli lista nie istnieje, zostanie utworzona. 
 */
+
 const addToLocalStorage = (id, listName) => {
   let storedList = localStorage.getItem('user');
   let parsedList = {};
@@ -22,3 +23,90 @@ const addToLocalStorage = (id, listName) => {
   localStorage.setItem('user', JSON.stringify(parsedList));
 };
 export { addToLocalStorage };
+
+/*
+import Notiflix from 'notiflix';
+const addToQueue = document.querySelector('.queue-btn');
+const addToWatched = document.querySelector('.watched-btn');
+
+let queueMovies = [];
+let watchedMovies = [];
+let movieId = [];
+let displayCard = '';
+
+const checkTheLocalStorage = event => {
+  if (event.target.className === 'movie-card__image') {
+    displayCard = event.target.parentNode.parentNode;
+    movieId = event.target.parentNode.children[1].children[2].textContent;
+    if (
+      queueMovies.length === 0 &&
+      JSON.parse(localStorage.getItem('added-to-queue')) !== null
+    ) {
+      queueMovies = JSON.parse(localStorage.getItem('added-to-queue'));
+    }
+
+    if (
+      watchedMovies.length === 0 &&
+      JSON.parse(localStorage.getItem('added-to-watched')) !== null
+    ) {
+      watchedMovies = JSON.parse(localStorage.getItem('added-to-watched'));
+    }
+  }
+  if (queueMovies !== null && queueMovies.includes(movieId)) {
+    addToQueue.textContent = 'removed form queue';
+  } else {
+    addToQueue.textContent = 'add to queue';
+  }
+
+  if (watchedMovies !== null && watchedMovies.includes(movieId)) {
+    addToWatched.textContent = 'removed form watched';
+  } else {
+    addToWatched.textContent = 'add to watched';
+  }
+};
+
+const queueStorage = event => {
+  if (!queueMovies.includes(movieId)) {
+    queueMovies.push(movieId);
+    localStorage.setItem('added-to-queue', JSON.stringify(queueMovies));
+  } else {
+    let deleteQueue = queueMovies.indexOf(movieId);
+    if (deleteQueue > -1) {
+      queueMovies.splice(deleteQueue, 1);
+      localStorage.setItem('added-to-queue', JSON.stringify(queueMovies));
+      addToQueue.textContent = 'add to queue';
+      return Notiflix.Notify.info('Movie removed from queue');
+    }
+  }
+  addToQueue.textContent = 'removed form queue';
+  return Notiflix.Notify.success('Movie added to queue');
+};
+
+const watchedStorage = event => {
+  if (watchedMovies === null) {
+    localStorage.setItem('added-to-watched', JSON.stringify(movieId));
+    watchedMovies.push(movieId);
+    addToWatched.textContent = 'removed form watched';
+    return Notiflix.Notify.success('Movie added to watched');
+  } else {
+    if (!watchedMovies.includes(movieId)) {
+      watchedMovies.push(movieId);
+      localStorage.setItem('added-to-watched', JSON.stringify(watchedMovies));
+    } else {
+      let deleteWatched = watchedMovies.indexOf(movieId);
+      if (deleteWatched > -1) {
+        watchedMovies.splice(deleteWatched, 1);
+        localStorage.setItem('added-to-watched', JSON.stringify(watchedMovies));
+        addToWatched.textContent = 'add to watched';
+        return Notiflix.Notify.info('Movie removed from watched');
+      }
+    }
+    addToWatched.textContent = 'removed form watched';
+    return Notiflix.Notify.success('Movie added to watched');
+  }
+};
+
+addEventListener('click', checkTheLocalStorage);
+addToQueue.addEventListener('click', queueStorage);
+addToWatched.addEventListener('click', watchedStorage);
+*/
--- src/js/localStorage/localStorageMod.js
+++ src/js/localStorage/localStorageMod.js
@@ -10,23 +10,63 @@ import jsLocalStorage from './jsLocalStorage';
 //3. zapisujemy otrzymany array w local storage
 
 // funkcja wywoływana przy uruchomieniu strony tworząca puste katalogi id
-const createLocalStorage = () => {} // ma do zrobienia stworzenie key=watched,queued // watched=[], queued=[] (format danych to array)
+const createLocalStorage = () => {
+    const watched = localStorage.getItem('watched');
+    const queued = localStorage.getItem('queued');
+     // Jeśli klucze nie istnieją, stworzenie nowych
+    if (!watched) {
+      jsLocalStorage.save('watched', []);
+    }
+    if (!queued) {
+      jsLocalStorage.save('queued', []);
+    }
+  }
+
+ // ma do zrobienia stworzenie key=watched,queued // watched=[], queued=[] (format danych to array)
 // musi też sprawdzić czy zmienne stnieją, żeby ich nie nadpisać 
 
 //argument arrayType - key który ma być użyty przy ładowaniu zasobu z local storage
 
 // funkcja wywoływana przy klikaniu na przyciski w modal (watched albo queued)
-const saveMovieId = (movieId, arrayType) => {} // DOPISUJE do istniejącego array [] movieId
+const saveMovieId = (movieId, arrayType) => {
+     // Pobranie array z local storage
+    const array = JSON.parse(jsLocalStorage.load(arrayType));
+    // Sprawdzenie, czy movieId już istnieje w array
+    if (array.includes(movieId)) {
+      return;
+    }
+}
+array.push(movieId);
+    // DOPISUJE do istniejącego array [] movieId
 // musi sprawdzić czy id już istnieje, jeśli istnieje to przerywa działanie funkcji
 // jeśli nie istnieje to robi push..
 
 // funkcja wywoływana przy klikaniu na przyciski w modal (watched albo queued)
-const removeMovieId = (movieId, arrayType) => {} // Jeśli znajdzie, wtedy usuwa ID z array. Jeśli nie znajdzie przerywa działanie
+const removeMovieId = (movieId, arrayType) => {
+    const array = JSON.parse(jsLocalStorage.load(arrayType));
+    const index = array.indexOf(movieId);
+    if (index === -1) {
+      return;
+    }
+}
+ // Usunięcie movieId z array
+array.splice(index, 1);
+ // Zapisanie zmienionego array do local storage
+jsLocalStorage.save(arrayType, JSON.stringify(array));
+    
+// Jeśli znajdzie, wtedy usuwa ID z array. Jeśli nie znajdzie przerywa działanie
 
 // funkcja wywoływana przy ładowaniu modala, oraz przy kliknięciu na przycisk (po dodaniu do localstorage)
 // w połączeniu z funkcją togglująca klasę w modalu (nie ma jeszcze takiej) służyłaby do zmiany koloru przycisku (jeśli mamy coś - przycisk pomarańczowy, jeśli nie przeźroczysty)
-const findMovieId = (movieId, arrayType) => {} // funkcja która tylko i wyłącznie znajduje movie ID i zwraca true albo false
+const findMovieId = (movieId, arrayType) => {
+    const array = JSON.parse(jsLocalStorage.load(arrayType));
+    // Sprawdzenie, czy movieId istnieje w array
+    return array.includes(movieId);
+  }
+
+ // funkcja która tylko i wyłącznie znajduje movie ID i zwraca true albo false
 
 
 const localStorageMod = { createLocalStorage, saveMovieId, removeMovieId, findMovieId };
 export default localStorageMod;
+

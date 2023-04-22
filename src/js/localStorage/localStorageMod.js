af911a566d61960f0a340135bec52e0ee2041348
MateuszCharysz <charyszek@interia.pl>
2023-04-22 09:53:28 +0200

    compleate ls not tested

--- src/js/localStorage/localStorageMod.js
+++ src/js/localStorage/localStorageMod.js
@@ -12,7 +12,10 @@ import jsLocalStorage from './jsLocalStorage';
 const cbfindId = (movieId, arrayType) => arrayType.includes(movieId); // funkcja cb - do sprawdzania id wewnątrz funkcji
 
 // funkcja wywoływana przy uruchomieniu strony tworząca puste katalogi id
-const createLocalStorage = () => {}; // ma do zrobienia stworzenie key=watched,queued // watched=[], queued=[] (format danych to array)
+const createLocalStorage = (arr1 = 'watched', arr2 = 'queued') => {
+  if (localStorage.getItem(arr1) === null) jsLocalStorage.save(arr1, []);
+  if (localStorage.getItem(arr2) === null) jsLocalStorage.save(arr2, []);
+}; // ma do zrobienia stworzenie key=watched,queued // watched=[], queued=[] (format danych to array)
 // musi też sprawdzić czy zmienne stnieją, żeby ich nie nadpisać
 
 //argument arrayType - key który ma być użyty przy ładowaniu zasobu z local storage
@@ -22,19 +25,27 @@ const saveMovieId = (movieId, arrayType) => {
   const arr = jsLocalStorage.load(arrayType);
   if (cbfindId(movieId, arr) !== true) {
     arr.push(movieId);
-    jsLocalStorage.save(arr);
+    jsLocalStorage.save(arrayType, arr);
   }
 }; // DOPISUJE do istniejącego array [] movieId
 // musi sprawdzić czy id już istnieje, jeśli istnieje to przerywa działanie funkcji
 // jeśli nie istnieje to robi push..
 
 // funkcja wywoływana przy klikaniu na przyciski w modal (watched albo queued)
-const removeMovieId = (movieId, arrayType) => {}; // Jeśli znajdzie, wtedy usuwa ID z array. Jeśli nie znajdzie przerywa działanie
+const removeMovieId = (movieId, arrayType) => {
+  const arr = jsLocalStorage.load(arrayType);
+  if (cbfindId(movieId, arr) === true) {
+    arr.splice(arr.indexOf(movieId), 1);
+    jsLocalStorage.save(arrayType, arr);
+  }
+}; // Jeśli znajdzie, wtedy usuwa ID z array. Jeśli nie znajdzie przerywa działanie
 
 // funkcja wywoływana przy ładowaniu modala, oraz przy kliknięciu na przycisk (po dodaniu do localstorage)
 // w połączeniu z funkcją togglująca klasę w modalu (nie ma jeszcze takiej) służyłaby do zmiany koloru przycisku (jeśli mamy coś - przycisk pomarańczowy, jeśli nie przeźroczysty)
-
-const findMovieId = (movieId, arrayType) => {}; // funkcja która tylko i wyłącznie znajduje movie ID i zwraca true albo false
+const findMovieId = (movieId, arrayType) => {
+  const arr = jsLocalStorage.load(arrayType);
+  return cbfindId(movieId, arr);
+}; // funkcja która tylko i wyłącznie znajduje movie ID i zwraca true albo false
 
 const localStorageMod = {
   createLocalStorage,
@@ -43,51 +54,3 @@ const localStorageMod = {
   findMovieId,
 };
 export default localStorageMod;
-
-/*
-const localStorageMod = {
-    createLocalStorage: function () {
-      // Sprawdzenie, czy klucze istnieją już w local storage
-      const watched = localStorage.getItem('watched');
-      const queued = localStorage.getItem('queued');
-      // Jeśli klucze nie istnieją, stworzenie nowych
-      if (!watched) {
-        localStorage.setItem('watched', JSON.stringify([]));
-      }
-      if (!queued) {
-        localStorage.setItem('queued', JSON.stringify([]));
-      }
-    },
-    saveMovieId: function (movieId, arrayType) {
-      // Pobranie array z local storage
-      const array = JSON.parse(localStorage.getItem(arrayType));
-      // Sprawdzenie, czy movieId już istnieje w array
-      if (array.includes(movieId)) {
-        return;
-      }
-      // Dodanie movieId do array
-      array.push(movieId);
-      // Zapisanie array do local storage
-      localStorage.setItem(arrayType, JSON.stringify(array));
-    },
-    removeMovieId: function (movieId, arrayType) {
-      // Pobranie array z local storage
-      const array = JSON.parse(localStorage.getItem(arrayType));
-      // Sprawdzenie, czy movieId istnieje w array
-      const index = array.indexOf(movieId);
-      if (index === -1) {
-        return;
-      }
-      // Usunięcie movieId z array
-      array.splice(index, 1);
-      // Zapisanie zmienionego array do local storage
-      localStorage.setItem(arrayType, JSON.stringify(array));
-    },
-    findMovieId: function (movieId, arrayType) {
-      // Pobranie array z local storage
-      const array = JSON.parse(localStorage.getItem(arrayType));
-      // Sprawdzenie, czy movieId istnieje w array
-      return array.includes(movieId);
-    },
-  };
-  export default localStorageMod;*/

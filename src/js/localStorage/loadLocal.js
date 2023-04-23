ebddccec78a4c3b4a27a1b0c88d5d090d0e046b9
potocki92 <mateusz.potocki92@gmail.com>
2023-04-23 12:58:48 +0200

    library
    Dodałem możliwość generowania galerii w momencie wciśnięcia przycisku 'watched' lub 'queue' na stronie library. Dane są pobierane z localStorage i poprzez 'fetchingMovieDetails' oraz 'updatingMovieHTML' generowane na stronie.
    W momencie kiedy uruchamiamy modal i usuwamy film z loclaStorage, karta z galerii jest również usuwana. Ponowne dodanie do localStorage powoduje pojawienie się karty w galerii  -> "movieBox.insertAdjacentHTML('beforeend', movieCard(data));"
    
    Miałem spory problem z użyciem "fetchingMovieDetails" oraz "updatingMovieHTML" w "openModal", aby dało mi możliwość dodawania karty do library w czasie rzeczywistym, oraz żeby nie ingerowało to w galerię w index. Użycie "updatingMovieHTML"  powodowało, że dodawało mi nowe karty do tego co zostało już wygenerowane, więc aby tego uniknąć pozwoliłem sobie dodać klasę w body "library-page" i za pomocą "const isLibraryPage = document.body.classList.contains('library-page')" sprawdzam czy jesteśmy na stronie z library.
    
    Dodałem nowy markup dla generowania pojedynczych kart w momencie dodawania filmów do localStorage.

--- src/js/localStorage/loadLocal.js
+++ src/js/localStorage/loadLocal.js
@@ -1,6 +1,8 @@
 import { fetchingMovieDetails } from '../gallery/fetchingMovieDetails';
-import { updatingMovieHTML } from '../gallery/galleryMarkup';
-import { cbClear, movieID } from '../gallery/galleryVariables';
+import {
+  updatingMovieHTML,
+} from '../gallery/galleryMarkup';
+import { cbClear, movieBox, movieID } from '../gallery/galleryVariables';
 
 let moviesId;
 const watchedButton = document.querySelector(
@@ -11,6 +13,8 @@ const queuedButton = document.querySelector('.library-button:last-of-type');
 watchedButton.addEventListener('click', async () => {
   cbClear();
   console.log('Watched');
+  document.body.classList.remove('queue-page')
+  document.body.classList.add('watched-page')
   moviesId = localStorage.getItem('watched');
   JSON.parse(moviesId).forEach(q => {
     movieID.push(q);
@@ -22,6 +26,8 @@ watchedButton.addEventListener('click', async () => {
 queuedButton.addEventListener('click', async () => {
   cbClear();
   console.log('Queued');
+  document.body.classList.remove('watched-page')
+  document.body.classList.add('queue-page')
   moviesId = localStorage.getItem('queued');
   JSON.parse(moviesId).forEach(q => {
     movieID.push(q);
---
+++ src/js/localStorage/movieCard.js
@@ -0,0 +1,26 @@
+export const movieCard = e => {
+    const genres = e.genres.map(genre => genre.name).join(', ');
+    const markup = `<div class="movie__card" data-id="${e.id}">
+   <div class="movie__imgbox">
+     <img class="movie__img" src="https://image.tmdb.org/t/p/w500${
+       e.poster_path
+     }"
+     alt="${e.title}" loading="lazy"/>
+   </div>
+   <p class="movie__title">
+     <b>${e.title}</b>
+   </p>
+   <div class="movie__info">
+     <p class="movie__genres">
+       ${genres.slice(0, 2)}&nbsp;
+     </p>
+     <p class="movie__year">
+       | ${''}
+     </p>
+   </div>
+  </div>
+   `;
+  
+    return markup;
+  };
+  

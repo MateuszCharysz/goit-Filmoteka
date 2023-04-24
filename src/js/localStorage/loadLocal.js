644c8cfe74afc2f1edd4515546681ff0822b7f1f
Mateusz Rybczynski <rybak.pl@gmail.com>
2023-04-24 10:09:53 +0200

    hilghted library buttons
    przyciski biblioteki zmieniają kolor przy zmianie z watched na queued, poprawiona animacja, oraz naprawiona szerokośc body na backdropie

--- src/js/localStorage/loadLocal.js
+++ src/js/localStorage/loadLocal.js
@@ -6,7 +6,7 @@ import localStorageMod from './localStorageMod';
 
 let moviesId;
 const watchedButton = document.querySelector(
-  'button.library-button:first-of-type',
+  'button.library-button:first-of-type'
 );
 const queuedButton = document.querySelector('.library-button:last-of-type');
 
@@ -16,6 +16,10 @@ watchedButton.addEventListener('click', async () => {
   console.log('Watched');
   document.body.classList.remove('queue-page');
   document.body.classList.add('watched-page');
+
+  watchedButton.classList.add('lib-btn--active');
+  queuedButton.classList.remove('lib-btn--active');
+
   moviesId = jsLocalStorage.load('watched');
   moviesId.forEach(q => {
     movieId.push(q);
@@ -25,12 +29,16 @@ watchedButton.addEventListener('click', async () => {
 });
 
 queuedButton.addEventListener('click', async () => {
-  localStorageMod.createLocalStorage()
+  localStorageMod.createLocalStorage();
   cbClear();
   console.log('Queued');
   document.body.classList.remove('watched-page');
   document.body.classList.add('queue-page');
   moviesId = jsLocalStorage.load('watched');
+
+  watchedButton.classList.remove('lib-btn--active');
+  queuedButton.classList.add('lib-btn--active');
+
   moviesId.forEach(q => {
     movieId.push(q);
   });

a78efad5ba37e23c26bbd218a8d474c823107355
MateuszCharysz <charyszek@interia.pl>
2023-04-24 20:15:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import showMovies from './js/gallery/renderGalleryMain';
 import showMoviesKeyWords from './js/gallery/renderGallerySearch';
 import { movieBox, cbClear } from './js/gallery/galleryVariables';
 import localStorageMod from './js/localStorage/localStorageMod';
+// import debounce from 'lodash.debounce';
 
 localStorageMod.createLocalStorage();
 //renderGallery => fetchingMovies => fetchingMovieDetails => galleryMarkup => pagination
@@ -11,11 +12,17 @@ showMovies();
 
 const button = document.getElementById('button');
 const searchInput = document.getElementById('search');
+// renderGallerySearch => fetchingMoviesSearch => fetchingMovieDetails => galleryMarkup => pagination
 
 button.addEventListener('click', event => {
   event.preventDefault();
   cbClear();
   showMoviesKeyWords(1, searchInput.value);
 });
+// button.addEventListener('click', showMoviesKeyWords);
+// searchInput.addEventListener(
+//   ('input', debounce(showMoviesKeyWords(1, searchInput.value), 300)),
+// );
 
+// openModal => fetchMovieById => modalMarkup
 movieBox.addEventListener('click', openModal);

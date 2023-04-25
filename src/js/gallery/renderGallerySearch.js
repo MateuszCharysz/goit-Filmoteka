c2febcc2bf9c81f3545aaf91a1e9fd0bb33e4416
MateuszCharysz <charyszek@interia.pl>
2023-04-25 19:00:26 +0200

    loader for LS

--- src/js/gallery/renderGallerySearch.js
+++ src/js/gallery/renderGallerySearch.js
@@ -16,24 +16,22 @@ const setMovieList = moviesData => {
 const showMoviesKeyWords = async (page, search) => {
   loader.classList.remove('loader--is-hidden');
   const moviesData = await fetchingMoviesSearch(page, search);
-
   const searchWarninng = document.querySelector('.header__search--warning');
-
   if (moviesData.results.length === 0) {
     searchWarninng.style.display = 'block';
+    loader.classList.add('loader--is-hidden');
   } else {
     searchWarninng.style.display = 'none';
+    setMovieList(moviesData);
+    await fetchingMovieDetails();
+    loader.classList.add('loader--is-hidden');
+    updatingMovieHTML();
+    const pagination = createPagination(moviesData);
+    pagination.on('beforeMove', ({ page }) => {
+      cbClear();
+      showMoviesKeyWords(page, search);
+    });
   }
-
-  setMovieList(moviesData);
-  await fetchingMovieDetails();
-  loader.classList.add('loader--is-hidden');
-  updatingMovieHTML();
-  const pagination = createPagination(moviesData);
-  pagination.on('beforeMove', ({ page }) => {
-    cbClear();
-    showMoviesKeyWords(page, search);
-  });
 };
 
 export default showMoviesKeyWords;
